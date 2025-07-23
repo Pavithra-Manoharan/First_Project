7ffbbb0402f7b4ec2daca20d9e57a72b370c34ce
Pavithra-Manoharan <pavimano1709@gmail.com>
2025-07-23 15:15:00 +0530

    filter method used to view active and inactive employees

--- query_implementation/viewEmp/viewEmployee.js
+++ query_implementation/viewEmp/viewEmployee.js
@@ -1,11 +1,28 @@
 import queries from '../../queries/queries.js';
 
-const viewEmployee = (req,res)=>{
-  req.app.locals.genericQuery(queries.viewEmployee,[],res);
+// const viewEmployee = (req,res)=>{
+//   req.app.locals.genericQuery(queries.viewEmployee,[],res);
+// }
+const viewEmployee = (req, res) => {
+  const {status:statusFilter} = req.params; 
+
+  req.app.locals.genericQuery(queries.viewEmployee, [], (err,results) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    if (statusFilter) {
+      const filtered = res.filter(emp => emp.status_name === statusFilter);
+      return res.status(200).json(filtered);
+    }
+
+    res.status(200).json(results); 
+  
+  });
 }
+
 const view_status_employee = (req, res) => {
-  const {status } = req.params;
+  const {status} = req.params;
    req.app.locals.genericQuery(queries.view_status_employee, [status], res);
     
 } 
-    export  {viewEmployee, view_status_employee};
+  export  {viewEmployee, view_status_employee};
+
